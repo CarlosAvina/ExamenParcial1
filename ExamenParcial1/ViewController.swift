4079c54db9ef17306c781a3dc719f0355e5d5f5b
Carlos Aviña <carlosavilu11@gmail.com>
2019-03-16 14:05:33 -0600

    Initial Commit

---
+++ ExamenParcial1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExamenParcial1
+//
+//  Created by Juan Carlos Aviña Luna on 3/16/19.
+//  Copyright © 2019 Juan Carlos Aviña Luna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
